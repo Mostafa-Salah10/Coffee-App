8b73e8ce4c13b010a906cc8a129c3ac0813e36bd
Mostafa Salah <e01006828614@gmail.com>
2025-04-21 01:56:47 +0200

    get all images in features

--- lib/core/utils/assets.dart
+++ lib/core/utils/assets.dart
@@ -1,7 +1,7 @@
 // ignore_for_file: prefer_single_quotes
 class Assets {
   Assets._();
-
+  
   /// Assets for assetsImagesCoffee1
   /// assets/images/coffee1.jpeg
   static const String assetsImagesCoffee1 = "assets/images/coffee1.jpeg";
@@ -16,17 +16,23 @@ class Assets {
 
   /// Assets for assetsImagesCoffeeLottie
   /// assets/images/coffeeLottie.json
-  static const String assetsImagesCoffeeLottie =
-      "assets/images/coffeeLottie.json";
+  static const String assetsImagesCoffeeLottie = "assets/images/coffeeLottie.json";
+
+  /// Assets for assetsImagesCupIcon
+  /// assets/images/cupIcon.PNG
+  static const String assetsImagesCupIcon = "assets/images/cupIcon.PNG";
 
   /// Assets for assetsImagesIstockphoto12676801111024x1024
   /// assets/images/istockphoto-1267680111-1024x1024.jpg
-  static const String assetsImagesIstockphoto12676801111024x1024 =
-      "assets/images/istockphoto-1267680111-1024x1024.jpg";
+  static const String assetsImagesIstockphoto12676801111024x1024 = "assets/images/istockphoto-1267680111-1024x1024.jpg";
 
   /// Assets for assetsImagesOnboarding
   /// assets/images/onboarding.jpeg
   static const String assetsImagesOnboarding = "assets/images/onboarding.jpeg";
+
+  /// Assets for assetsImagesOnboardingImage
+  /// assets/images/onboardingImage.PNG
+  static const String assetsImagesOnboardingImage = "assets/images/onboardingImage.PNG";
 
   /// Assets for assetsImagesTea1
   /// assets/images/tea1.jpeg
@@ -34,6 +40,6 @@ class Assets {
 
   /// Assets for assetsImagesWebsiteDesign
   /// assets/images/website_design.jpeg
-  static const String assetsImagesWebsiteDesign =
-      "assets/images/website_design.jpeg";
+  static const String assetsImagesWebsiteDesign = "assets/images/website_design.jpeg";
 }
+
